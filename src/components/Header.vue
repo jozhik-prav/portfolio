24e6f70b3765b8cb6cee27d28bab5b53c65f4378
Jozhik_Prav <grahova_sasha@mail.ru>
2020-06-08 18:29:02 +0300

    Шапка сайта

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+    <v-toolbar
+      dense
+      dark
+    >
+      <v-toolbar-title>Александра Грахова</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items><v-btn text>Опыт работы</v-btn></v-toolbar-items>
+
+      <v-toolbar-items><v-btn text>Профессиональные навыки</v-btn></v-toolbar-items>
+
+      <v-toolbar-items><v-btn text>Портфолио</v-btn></v-toolbar-items>
+
+    </v-toolbar>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class LobbyList extends Vue {
+    
+}
+</script>
