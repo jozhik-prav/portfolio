99ca594e98e2528b98f1d7483ee32305c13f4091
Jozhik_Prav <grahova_sasha@mail.ru>
2020-06-08 20:39:09 +0300

    Сделала первый экран

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
-  <Header></Header>
+  <div>
+    <v-app>
+      <Header></Header>
+      <main>
+          <Home></Home>
+      </main>
+    </v-app>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Header from "./components/Header.vue";
+import Home from "./components/Home.vue"
 
 export default Vue.extend({
   name: "App",
 
   components: {
-    Header
+    Header,
+    Home
   },
 
   data: () => ({
---
+++ src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="home">
+        <v-container fluid fill-height>
+            <v-row align="center">
+                <v-col>
+                    <h1 class="title">Junior Frontend<br />разработчик</h1>
+                </v-col>
+            </v-row>
+        </v-container>
+        
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Home extends Vue {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .home {
+        height: calc(100vh - 118px);
+        max-height: 970px;
+        background: #ffffff url('../assets/home.png') bottom right no-repeat;
+        background-size: 60%;
+        position: relative;
+    
+        .title {
+            text-transform: uppercase;
+            font-size: 44px !important;
+            line-height: 1;
+            color: #e93f7d;
+            padding-left: 10%;
+        }
+    }
+</style>
